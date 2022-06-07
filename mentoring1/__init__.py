bb0d43fd5c4f7a62b4a799ca78927106ac77b9df
buettnerb <ben.buettner@gmx.de>
2022-06-07 18:43:09 +0200

    Group-matching approach

--- mentoring1/__init__.py
+++ mentoring1/__init__.py
@@ -28,18 +28,23 @@ class Player(BasePlayer):
     testquest = models.IntegerField(
         label='Please give me a number.'
     )
-    feedback = models.StringField()
-    promotion = models.StringField()
-    promotion2 = models.StringField()
+    feedback = models.LongStringField()
+    promotion = models.LongStringField()
+    promotion2 = models.LongStringField()
 
 
 # Methods
 def creating_session(subsession: Subsession):
+    session = subsession.session
+
     treats = itertools.cycle(['t1', 't2', 't3', 't4'])
     # players per treatment 2, 3 and 4
     p_per_treat = 90
     # mentors per treatment 2, 3 and 4
     m_per_treat = 10
+
+    # assign players to treatments; treatments 2-4 will have p_per_treat players max, the excess will be assigned to
+    # treatement 1:
     t1_players = []
     t2_players = []
     t3_players = []
@@ -65,13 +70,73 @@ def creating_session(subsession: Subsession):
         p.job = 'worker'
         p.participant.job = 'worker'
 
+    print(len(t2_players))
+
+    # out of the players of the treatments respectively, m_per_treat mentors are randomly chosen
     t2_mentors = random.sample(t2_players, m_per_treat)
+    # list that only contains workers is created:
+    t2_workers = t2_players
+    for e in t2_workers:
+        if e in t2_mentors:
+            t2_workers.remove(e)
+    print(len(t2_workers))
+    print(len(t2_mentors))
+
+    # same for other treatments
     t3_mentors = random.sample(t3_players, m_per_treat)
+    t3_workers = t3_players
+    for e in t3_workers:
+        if e in t3_mentors:
+            t3_workers.remove(e)
+
     t4_mentors = random.sample(t4_players, m_per_treat)
+    t4_workers = t4_players
+    for e in t4_workers:
+        if e in t4_mentors:
+            t4_workers.remove(e)
+
+    # all mentors are assigned mentor job
     mentors = itertools.chain(t2_mentors, t3_mentors, t4_mentors)
     for p in mentors:
         p.job = 'mentor'
         p.participant.job = 'mentor'
+
+    # TODO: group assignment for treatment 2:
+    #  no errors but doesn't work the way I want it to yet
+    t2_groups = {}
+    groupids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    gid = 1
+    t2_w_help = t2_workers
+    #print(len(t2_w_help))
+    while gid < (m_per_treat + 1):
+        nextgroup = []
+        nextgroup.append(t2_mentors[gid-1])
+
+        ws = random.sample(t2_w_help, 8)
+        for e in t2_w_help:
+            if e in ws:
+                t2_w_help.remove(e)
+        nextgroup.extend(ws)
+        t2_groups["{}".format(gid)] = nextgroup
+        gid = gid + 1
+    #print(t2_groups)
+    #print(len(t2_w_help))
+    #print(len(t2_groups))
+    #for id in groupids:
+    #    print(len(t2_groups["{}".format(id)]))
+
+    # TODO: add dicts and lists to session.vars
+
+    #session.t1_players = t1_players
+    #session.t2_players = t2_players
+    #session.t3_players = t3_players
+    #session.t4_players = t4_players
+    #session.t2_workers = t2_workers
+    #session.t3_workers = t2_workers
+    #session.t4_workers = t2_workers
+    #session.t2_mentors = t2_mentors
+    #session.t3_mentors = t2_mentors
+    #session.t4_mentors = t2_mentors
 
 
 # PAGES
@@ -99,9 +164,13 @@ class MentorTask(Page):
     form_fields = [
         'feedback'
     ]
+
     @staticmethod
     def is_displayed(player: Player):
         return player.job == 'mentor'
+
+    def before_next_page(player: Player, timeout_happened):
+        pass
 
 
 class Promotion(Page):
@@ -109,6 +178,7 @@ class Promotion(Page):
     form_fields = [
         'promotion'
     ]
+
     @staticmethod
     def is_displayed(player: Player):
         return player.job == 'worker'
@@ -119,6 +189,7 @@ class PromotionII(Page):
     form_fields = [
         'promotion2'
     ]
+
     @staticmethod
     def is_displayed(player: Player):
         return (player.treat == 't3' or player.treat == 't4') and player.job == 'worker'
